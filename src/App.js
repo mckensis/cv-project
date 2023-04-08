be2cc968f64cdd70850f3e0402abe80b72429b5f
Aidan Mckenzie <adn.mck@gmail.com>
2023-04-09 00:50:48 +0100

    moved add section buttons to main component

--- src/App.js
+++ src/App.js
@@ -13,21 +13,33 @@ import './stylesheets/Projects.css';
 import './stylesheets/Skills.css';
 import './stylesheets/Work.css';
 import './stylesheets/Print.css';
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function App() {
+  const [aboutSectionEnabled, setAboutSectionEnabled] = useState(false);
+  const [skillsSectionEnabled, setSkillsSectionEnabled] = useState(false);
+  const [projectsSectionEnabled, setProjectsSectionEnabled] = useState(false);
+  const [workSectionEnabled, setWorkSectionEnabled] = useState(false);
+  const [educationSectionEnabled, setEducationSectionEnabled] = useState(false);
   const mainRef = useRef();
-  
+
   return (
     <div className="App">
       <Header main={mainRef} />
       <main id="main" ref={mainRef}>
+        {/* Buttons to enable various sections */}
         <Personal />
-        <About />
-        <Skills />
-        <Projects />
-        <Work />
-        <Education />
+        {!aboutSectionEnabled && <button className="add" onClick={() => setAboutSectionEnabled(true)}>Add About Section</button>}
+        {!skillsSectionEnabled && <button className="add" onClick={() => setSkillsSectionEnabled(true)}>Add Key Skills Section</button>}
+        {!projectsSectionEnabled && <button className="add" onClick={() => setProjectsSectionEnabled(true)}>Add Projects Section</button>}
+        {!workSectionEnabled && <button className="add" onClick={() => setWorkSectionEnabled(true)}>Add Work Section</button>}
+        {!educationSectionEnabled && <button className="add" onClick={() => setEducationSectionEnabled(true)}>Add Education Section</button>}
+        
+        {aboutSectionEnabled && <About />}
+        {skillsSectionEnabled && <Skills />}
+        {projectsSectionEnabled && <Projects />}
+        {workSectionEnabled && <Work />}
+        {educationSectionEnabled && <Education />}
       </main>
     </div>
   );
